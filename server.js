f7d73786606699e04aa406ad7a2346dad048a3aa
kovendan16 <kovendan16@gmail.com>
2023-04-18 15:44:19 +0530

    added

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const port = 5000;
+const bodyparser = require("body-parser");
+const path = require("path");
+const nodemailer = require("nodemailer");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("/index.html", { root: "public" });
+});
+
+app.post("/send_email", function (req, response) {
+  const output = `
+  <p>You have a new contact request</p>
+  <h3>Contact Details</h3>
+  <ul>  
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+  </ul>
+  <h3>Message:${req.body.Subject}</h3>
+  <h3>SUbject:${req.body.Message}</h3>
+
+  `;
+  console.log(req.body);
+
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  var mailOptions = {
+    //    from: "kovendan16@gmail.com",//
+    to: process.env.EMAIL,
+    subject: "from kovendan",
+    html: output,
+    // cc:"kovendan16@gmail.com",//
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      return res.send("unable to try later");
+    } else {
+      console.log("Email Sent: " + info.response);
+    }
+    response.send("message sent sucuessfully");
+  });
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
