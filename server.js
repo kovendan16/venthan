2e3d51e837fa9d7147714e78af01d118b47f488b
kovendan16 <kovendan16@gmail.com>
2023-04-20 11:19:18 +0530

    details added and aler box updated

--- server.js
+++ server.js
@@ -21,15 +21,14 @@ app.get("/", (req, res) => {
 
 app.post("/send_email", function (req, response) {
   const output = `
-  <p>You have a new contact request</p>
-  <h3>Contact Details</h3>
-  <ul>  
-    <li>Name: ${req.body.name}</li>
-    <li>Email: ${req.body.email}</li>
-  </ul>
-  <h3>Message:${req.body.Subject}</h3>
-  <h3>SUbject:${req.body.Message}</h3>
-
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message:${req.body.Subject}</h3>
+    <h3>SUbject:${req.body.Message}</h3>
   `;
   console.log(req.body);
 
@@ -43,23 +42,27 @@ app.post("/send_email", function (req, response) {
   });
 
   var mailOptions = {
-    //    from: "kovendan16@gmail.com",//
     to: "kovendan16@gmail.com",
     subject: "from kovendan",
     html: output,
-    // cc:"kovendan16@gmail.com",//
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(`${error}`);
-      return res.send("unable to try later");
+      const message = `sorry unable to sent ${error}`;
+      const script = `alert('${message}'); window.location.href='/';`;
+
+      return response.send(`<script>${script}</script>`);
     } else {
       console.log("Email Sent: " + info.response);
+      const message = "Message sent successfully!";
+      const script = `alert('${message}'); window.location.href='/';`;
+      response.send(`<script>${script}</script>`);
     }
-    response.send("message sent sucuessfully");
   });
 });
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`);
 });
